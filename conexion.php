0e3c634710d8e0e3518d7dfd1db21891e6cb7fe7
Your Name <sofiaevangelinaa@.com>
2024-10-04 14:34:07 -0300

    hola

---
+++ conexion.php
@@ -0,0 +1,10 @@
+<?
+$host = "localhost";
+$usuario= "root"; 
+$contra = "";
+$bd = "programacion"; 
+
+$con = new mysqli($host, $usuario, $contra, $bd); 
+
+
+?> 
---
+++ registro.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action= "guardar.php" method= "post">  
+nombre <br>
+<input type="text" name="nombre" id=""> <br>
+contraseña <br>
+<input type="password" name="contrasena" id=""> <br> 
+fecha <br>
+<input type=" date" name="fecha" id=""> <br>
+foto <br>
+<input type="file" name="foto" id=""> 
+<br>
+<button type="sumit" >  Registrado </button>
+
+
+</form>    
+
+
+
+</body>
+</html>
